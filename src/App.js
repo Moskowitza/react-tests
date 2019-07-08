b819cf77738f6a7c9f62e444f98025d89cd74bb6
moskowitza <moskowitzaaron@mac.com>
2019-07-08 18:01:01 -0400

    working on app deploy

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import Movie from "./components/Movie/Movie.js"
 
-function App() {
-  return (
+import MoviesList from './components/MoviesList/MoviesList';
+import MovieDetail from './components/MovieDetail/MovieDetail';
+
+const App = () => (
+  <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Link>
       </header>
+      <Switch>
+        <Route exact path="/" component={MoviesList} />
+        <Route path="/:id" component={MovieDetail} />
+      </Switch>
     </div>
-  );
-}
+  </Router>
+);
 
 export default App;
