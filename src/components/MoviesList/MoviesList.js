177d28c760b8bb230de4dddd25484f4e0ae390ae
moskowitza <moskowitzaaron@mac.com>
2019-07-08 18:08:11 -0400

    path to keys must be in src

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -2,7 +2,7 @@
 import React, { PureComponent } from 'react';
 import styled from 'styled-components';
 import Movie from '../Movie/Movie';
-
+import {apiKey} from '../../keys/apiKeys'
 class MoviesList extends PureComponent {
   state = {
     movies: [],
@@ -11,7 +11,7 @@ class MoviesList extends PureComponent {
   async componentDidMount() {
     try {
       const res = await fetch(
-        'https://api.themoviedb.org/3/discover/movie?api_key=hi&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1',
+        `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`,
       );
       const movies = await res.json();
       if(movies.success){
